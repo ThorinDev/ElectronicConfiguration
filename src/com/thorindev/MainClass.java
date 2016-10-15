d3e46130c6b90172b9b386c18a4041cbf0609c38
Chris Nethercott <c.nethercott@icloud.com>
2016-10-15 14:25:11 +0100

    Added an example to the main class

--- src/com/thorindev/MainClass.java
+++ src/com/thorindev/MainClass.java
@@ -5,7 +5,9 @@ public class MainClass {
 	static ElectronConfig ec = new ElectronConfig();
 
 	public static void main(String[] args) {
-		
+		for(int i = 1; i <= 50; i++) { //This will cycle through the first 50 atoms and return their electronic config
+			System.out.println(ec.electronicConfig(i));
+		}
 	}
 
 }
